a1cee2e9566b9c15230e763e91c81a1c9ec92232
Демянюк ВВ <v.demyanyuk@rusbitech.ru>
2024-06-14 16:17:38 +0300

    Конструкторы класса.
    Signed-off-by: Демянюк ВВ <v.demyanyuk@rusbitech.ru>

--- app/src/main/java/com/example/listapp/MainActivity.kt
+++ app/src/main/java/com/example/listapp/MainActivity.kt
@@ -5,14 +5,28 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.listapp.databinding.ActivityMainBinding
 
+class InitOrderDemo(name: String) {
+    val firstProperty = "Первое свойство: $name".also(::println)
+
+    val secondProperty = "Второе свойство: ${name.length}".also(::println)
+
+    init {
+        println("Первый блок инициализации: ${name}")
+    }
+
+    init {
+        println("Второй блок инициализации: ${name.length}")
+    }
+}
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bindingClass: ActivityMainBinding
+    //private lateinit var bindingClass: ActivityMainBinding
+    var test = InitOrderDemo("test")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        bindingClass = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindingClass.root)
+        //bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(bindingClass.root)
     }
 }
