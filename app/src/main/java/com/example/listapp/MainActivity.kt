39c5a79925754a889c35c2b315aa09f11e0c22f0
Демянюк ВВ <v.demyanyuk@rusbitech.ru>
2024-07-05 15:32:53 +0300

    05.06.2024 Решение обработки событий RadioButton.
    Signed-off-by: Демянюк ВВ <v.demyanyuk@rusbitech.ru>

--- app/src/main/java/com/example/listapp/MainActivity.kt
+++ app/src/main/java/com/example/listapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.listapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.listapp.databinding.ActivityMainBinding
 
@@ -19,14 +21,24 @@ class InitOrderDemo(name: String) {
     }
 }
 class MainActivity : AppCompatActivity() {
-
-    //private lateinit var bindingClass: ActivityMainBinding
+    private lateinit var bindingClass: ActivityMainBinding
     var test = InitOrderDemo("test")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //bindingClass = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(bindingClass.root)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+    }
+
+    fun onClickRB(view: View) {
+
+        var tmp: RadioButton
+
+        for (rb in bindingClass.group1.referencedIds) {
+            tmp = findViewById<RadioButton>(rb)
+            if (rb != view.id && tmp.isChecked())
+                tmp.setChecked(false)
+        }
     }
 }
